5003395aefdd6fb02e13479caa2775b66240764d
eeshankkhatri07 <147783447+eeshankkhatri07@users.noreply.github.com>
2025-03-18 11:28:20 +0530

    Time: 1321 ms (12.19%) | Memory: 31.8 MB (71.04%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        left,right=0,len(nums)
+        result=(1)
+        while left<=right:
+            length=left+(right-left)//2
+            if self._form_nice_subarray(length,nums):
+                result=length
+                left=length+1
+            else:
+                right=length-1
+        return result
+    def _form_nice_subarray(self,length:int,nums:list[int])->bool:
+        if length<=1:
+            return True
+        for start in range(len(nums)-length+1):
+            bit_mask=0
+            is_nice=True 
+            for pos in range(start,start+length):
+                if bit_mask & nums[pos]!=0:
+                    is_nice=False
+                    break
+                bit_mask |=nums[pos]
+            if is_nice:
+                return True
+        return False
+        
